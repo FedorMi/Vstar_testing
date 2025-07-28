d6083914236d3f679fba34398bad5ff5062d3e25
FedorMi <f.miloglyadov@gmail.com>
2025-07-28 10:20:21 +0200

    Fixed combined IVM bbox gen

--- visual_search_replacements/IVM_test_boxes.py
+++ visual_search_replacements/IVM_test_boxes.py
@@ -1,4 +1,6 @@
-from IVM import load, forward_batch
+from IVM import load, forward_batch, auto_postprocess
+from typing import Type, Tuple, List
+from torch.nn import functional as F
 
 import numpy as np
 import json
@@ -23,6 +25,44 @@ def initialize_and_check_model(ckpt_path, low_gpu_memory=False):
     
     print(f"Total Model Size: {total_size / (1024 ** 2):.2f} MB ({total_size / (1024 ** 3):.2f} GB)")
     return model
+@torch.no_grad()
+def forward_batch_intermediate(
+    model, 
+    image, # list of PIL.Image
+    instruction: List[str], # list of instruction
+    threshold: float = 0.1, # threshold for pixel reserve/drop
+    do_crop = False,
+    overlay_color = (255,255,255)
+):
+    ori_sizes = [img.size for img in image]
+    ori_images = [np.asarray(img).astype(np.float32) for img in image]
+    masks = model.generate_batch([img.resize((1024, 1024)) for img in image], instruction)
+
+    result = []
+    for mask, ori_image, ori_size in zip(masks, ori_images, ori_sizes):
+        mask = torch.sigmoid(F.interpolate(
+            mask.unsqueeze(0),
+            (ori_size[1], ori_size[0]),
+            mode="bilinear",
+            align_corners=False,
+        )[0, 0, :, :]).detach().cpu().numpy().astype(np.float32)[:,:,np.newaxis]
+        if threshold > mask.max(): mask += threshold # fail to find the target, reserve the full image
+        mask = auto_postprocess((mask > threshold).astype(np.float32))
+
+        if len(ori_image.shape) < 3: ori_image = ori_image[:,:,np.newaxis].repeat(3,-1)
+        
+        processed_image = ori_image * mask + torch.tensor(overlay_color, dtype=torch.uint8).repeat(ori_size[1], ori_size[0], 1).numpy() * (1 - mask)
+        temp = torch.tensor(overlay_color, dtype=torch.uint8).repeat(ori_size[1], ori_size[0], 1).numpy()
+        try:
+            y_indices, x_indices = np.where(mask[:,:,0] > 0)
+            x_min, x_max = x_indices.min(), x_indices.max()
+            y_min, y_max = y_indices.min(), y_indices.max()
+            processed_image = processed_image[y_min:y_max+1, x_min:x_max+1]
+        except:
+            print("Warning, unable to crop a sample, reserve whole image")
+        result.append(processed_image)
+        return processed_image, ori_image, mask, temp, x_min, x_max, y_min, y_max
+    return result
 
 def main_normal():
     ckpt_path = os.path.join("pytorch_model", "IVM-V1.0.bin") # your model path here
@@ -64,17 +104,24 @@ def main_test():
             data1 = load_json(full_json_path)
             images_related = data1["target_object"]
             instruction = data1["question"]        
-            image = Image.open(image_path) # your image path
-            _, temp_image, temp_mask, temp_temp = forward_batch(model, [image], [instruction], threshold = 0.8)
+            image = Image.open(full_image_path) # your image path
+            _, temp_image, temp_mask, temp_temp, o1, o2, o3, o4 = forward_batch_intermediate(model, [image], [instruction], threshold = 0.8)
             temp_masks = [temp_mask]
             for j in images_related:
                 print(j)
-                _, _, temp_mask, _, o1, o2, o3, o4 = forward_batch(model, [image], ["Please locate the"+j+"in this image."], threshold = 0.8)
+                _, _, temp_mask, _, o1, o2, o3, o4 = forward_batch_intermediate(model, [image], ["Please locate the"+j+"in this image."], threshold = 0.8)
                 temp_masks.append(temp_mask)
             mask = np.zeros_like(temp_masks[0])
             for temp_mask in temp_masks:
                 mask = np.maximum(mask,temp_mask)
             result = temp_image * mask + temp_temp * (1 - mask)
+            try:
+                y_indices, x_indices = np.where(mask[:,:,0] > 0)
+                x_min, x_max = x_indices.min(), x_indices.max()
+                y_min, y_max = y_indices.min(), y_indices.max()
+                result = result[y_min:y_max+1, x_min:x_max+1]
+            except:
+                print("Warning, unable to crop a sample, reserve whole image")
             # Convert the result to an image and save it
             output_image = Image.fromarray((result).astype(np.uint8))
             output_path = os.path.join("ivm_image_results_related", test_type, i)
