7d2b3a4faef7f18ae655cfc6820c6fd272e95f2d
FedorMi <f.miloglyadov@gmail.com>
2025-07-29 20:01:18 +0200

    new idea for textgrad

--- textgrad_testing/object_recall_grad_descent_ollama.py
+++ textgrad_testing/object_recall_grad_descent_ollama.py
@@ -197,3 +197,40 @@ def optimize_prompt_ollama_image_first(model_fn: Callable[[str, Any], Any], mode
     print("Final optimized prompt:", prompt.value)
 
     return prompt.value
+
+def optimize_prompt_ollama_last_call(model_fn: Callable[[str, Any], Any], initial_prompt_set: List[str], steps: int = 3):
+    client = OpenAI(base_url="http://localhost:11434/v1", api_key="ollama")
+    engine = ChatExternalClient(client=client, model_string='llama3:70b')
+
+    tg.set_backward_engine(engine, override=True)
+
+    prompt = tg.Variable(value=initial_prompt_set[0], requires_grad=True, role_description="prompt to optimize")
+    optimizer = tg.TGD(parameters=[prompt])
+
+    # System prompt for feedback-based loss
+    loss_system_prompt = tg.Variable(
+        """You are a helpful assistant. Evaluate the model's performance for each of the prompts and try to identify a pattern for what makes a good prompt template for this task. Provide concise feedback on what parts of the prompt contribute to a successful outcome, and suggest improvements for only the prompt template. Do not solve the task yourself, do not create a new prompt template yourself, only provide feedback on how to improve the prompt template to better achieve a result.""",
+        requires_grad=False,
+        role_description="system prompt for feedback loss"
+    )
+    text_loss = tg.TextLoss(loss_system_prompt)
+        
+    for _ in range(steps):
+        losses = []
+        feedback_text = ""
+        for k in range(len(initial_prompt_set)):
+            print("current prompt:", prompt.value)
+            optimizer.zero_grad()
+            model_output = model_fn(prompt.value)
+            #feedback = f"Prompt Template To Improve: {prompt.value}\nInput: {inp}\nExpected: {expected}\nModel Output: {model_output}"
+            feedback = f"Prompt Template:{prompt.value}\nModel Performance: {model_output}\n\n"
+            feedback_text += feedback
+            print(f"Feedback Text: {feedback_text}")
+        elge = tg.Variable(feedback_text, requires_grad=True, role_description="feedback for prompt")
+        loss = text_loss(elge)
+        print(f"Loss: {loss}")
+        loss.backward()
+        optimizer.step()
+    print("Final optimized prompt:", prompt.value)
+
+    return prompt.value
