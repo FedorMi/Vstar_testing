72283a7d1c21d92c50e368e4083062bd9ac0f3e7
FedorMi <f.miloglyadov@gmail.com>
2025-08-19 12:01:19 +0200

    additional for evaluation

---
+++ prompt_optim_outputs/initial_ten/evaluate_file.py
@@ -0,0 +1,32 @@
+import json
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+temp = "eval_result_manual_results"
+json_files = [temp + str(idx) + ".json" for idx in range(1, 11)]
+
+for i in json_files:
+    path = os.path.join("dino_jsons", i)
+    path = os.path.join("visual_search_replacements", path)
+    name = i.split(".")[0].replace("eval_results_", "")
+    with open(path, "r") as f:
+        hyperparameters = json.load(f)
+    direct_attributes_correct = 0
+    relative_position_correct = 0
+    direct_attributes_count = 0
+    relative_position_count = 0
+    count = 0
+    for j in hyperparameters:
+        for k in hyperparameters[j]:
+            if j == "direct_attributes":
+                direct_attributes_correct += k["correct"]
+                direct_attributes_count += 1
+            if j == "relative_position":
+                relative_position_correct += k["correct"]
+                relative_position_count += 1
+    overall_correct = direct_attributes_correct + relative_position_correct
+    overall_count = direct_attributes_count + relative_position_count
+    print("direct_attributes:", direct_attributes_correct / direct_attributes_count * 100, "%")
+    print("relative_position:", relative_position_correct / relative_position_count * 100, "%")
+    print("overall:", overall_correct / overall_count * 100, "%")
