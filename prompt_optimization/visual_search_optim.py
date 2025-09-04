b1968ce49cac9090e7c352216c891ce031e4f353
Fedor M <37484405+FedorMi@users.noreply.github.com>
2025-09-04 11:57:37 +0200

    Add comment about prompt testing and code origin
    Added a comment indicating the origin and purpose of the modifications.

--- prompt_optimization/visual_search_optim.py
+++ prompt_optimization/visual_search_optim.py
@@ -1,3 +1,5 @@
+# as this is a modified visual search file to allow for prompt testing on V*, almost the whole code is from https://github.com/penghao-wu/vstar
+
 import argparse
 import os
 import sys
